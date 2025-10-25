3525e94f61b9f16f8deaea8a094ada4a9f780828
ufreife85 <ufreife85@gmail.com>
2025-10-25 09:17:33 -0400

    unlock 23

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const units = [
     title: "Unit 2 — Motion in One Dimension",
     items: [
       { name: "2.1–2.2 Position, Distance, Displacement & Avg Velocity", href: "./unitLessons/index.html#/P2_1_2positionVelocity?view=overview", password: ""},
-      { name: "2.3 Instantaneous Velocity", href: "./unitLessons/index.html#/P2_3instantaneousVelocity?view=overview", password: "phys23" },
+      { name: "2.3 Instantaneous Velocity", href: "./unitLessons/index.html#/P2_3instantaneousVelocity?view=overview", password: "" },
       { name: "2.4 Average Acceleration", href: "./unitLessons/index.html#/P2_4averageAcceleration?view=overview", password: "phys24"},
       { name: "2.5 Uniform (Constant) Acceleration", href: "./unitLessons/index.html#/P2_5uniformAcceleration?view=overview", password: "phys25" },
       { name: "2.6 Displacement with Constant Acceleration", href: "./unitLessons/index.html#/P2_6displacementConstAccel?view=overview", password: "phys26" }
